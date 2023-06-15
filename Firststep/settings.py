79c8dace44b6c1fc77deca84c4864adae460f50f
Michael Adawaren <gadocer54@gmail.com>
2023-06-15 22:42:27 +0100

    Changes to be committed: 	modified:   Firststep/__pycache__/settings.cpython-310.pyc 	modified:   Firststep/settings.py

--- Firststep/settings.py
+++ Firststep/settings.py
@@ -133,13 +133,13 @@ import os
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 MEDIA_URL = '/images/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
