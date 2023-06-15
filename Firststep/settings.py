4dd5de0e43331cf30344f081086d8999acaf03ac
Michael Adawaren <gadocer54@gmail.com>
2023-06-15 22:35:23 +0100

    Changes to be committed: 	modified:   Firststep/settings.py

--- Firststep/settings.py
+++ Firststep/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
