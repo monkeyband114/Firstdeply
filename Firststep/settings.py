ee96719942c6aa0afe827f0c51c445589b1a3a30
Michael Adawaren <gadocer54@gmail.com>
2023-06-16 00:36:04 +0100

    Changes to be committed: 	modified:   Firststep/__pycache__/settings.cpython-310.pyc 	modified:   Firststep/settings.py

--- Firststep/settings.py
+++ Firststep/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'schoolmgt/Templates')
+            os.path.join(BASE_DIR, 'schoolmgt/Templates'),
             os.path.join(BASE_DIR, 'Templates'),
             
             ],
